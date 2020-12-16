e25dfd13e42b7fa09d9df661d3be644107e74ae6
William Spencer <william.spencer@simplybusiness.com>
2020-12-16 16:32:30 +0000

    Code from first breakout session

---
+++ models/orders.sql
@@ -0,0 +1,43 @@
+with 
+
+orders as (
+
+  select * from {{ref('stg_orders')}}
+
+),
+
+payments as (
+
+  select * from {{ref('stg_payments')}}
+
+),
+
+order_payments as (
+
+    select 
+
+        order_id,
+        sum(case when status = 'success' then amount end) as amount
+
+    from payments
+    group by 1
+
+),
+
+final as (
+
+    select 
+
+        orders.order_id,
+        orders.customer_id,
+        orders.order_date,
+        orders.status,
+        order_payments.amount
+
+    from orders
+    left join order_payments 
+        on orders.order_id = order_payments.order_id
+
+)
+
+select * from  final
