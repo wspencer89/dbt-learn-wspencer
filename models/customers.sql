97bd10cef27aab670e5d6b2b5552bfd112be0396
William Spencer <william.spencer@simplybusiness.com>
2020-12-16 20:26:40 +0000

    adding sources and tests

--- models/customers.sql
+++ models/customers.sql
@@ -1,10 +1,19 @@
 with customers as (
+
     select * from {{ ref('stg_customers')}}
+
 ),
+
+
 orders as (
+
     select * from {{ ref('orders')}}
+
 ),
+
+
 customer_orders as (
+
     select
         customer_id,
         min(order_date) as first_order_date,
@@ -14,8 +23,12 @@ customer_orders as (
         
     from orders
     group by 1
+
 ),
+
+
 final as (
+
     select
         customers.customer_id,
         customers.first_name,
@@ -25,6 +38,10 @@ final as (
         coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
         customer_orders.lifetime_value
     from customers
-    left join customer_orders using (customer_id)
+    left join customer_orders 
+        on customers.customer_id = customer_orders.customer_id
+
 )
+
+
 select * from final
